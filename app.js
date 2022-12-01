7897729e09829c4c5f8e148f27c8c2ce38178203
Yash Kaskar <yash.kaskar99@gmail.com>
2022-12-01 13:00:27 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,5 @@
+import index from './index';
+
+app.listen(8080, () => {
+    console.log("Listing On Port 8080")
+})
---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+
+// app.use((req, res) => {
+//     console.log("We got a new request");
+//     res.send('<h1> Welcome to webPage </h1>')
+// })
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+
+app.get('/dog', (req, res) => {
+    res.send('Woof')
+})
+
+app.get('/cats', (req, res) => {
+    res.send('Meowww')
+})
+
+
+app.get('/cycle', (req, res) => {   
+    res.send('Lets go for a ride')
+})
+
+module.exports = index;
